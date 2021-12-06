476bd01bf8281936ab8a506db207a3dbe73b4562
aduditsky <60918075+aduditsky@users.noreply.github.com>
2021-12-07 04:19:54 +0700

    QR Code

--- src/components/Iframe/Iframe.js
+++ src/components/Iframe/Iframe.js
@@ -3,17 +3,17 @@ import css from './iframe.module.css';
 
 const Iframe = ({ source }) => {
   if (!source) {
-    return;
+    return <></>;
   }
 
-  const src = source;
   return (
     <div className={css.iframeContainer}>
       <iframe
+        title='CovidCertificate'
         frameBorder='0'
         scrolling='no'
         className={css.iframeBlock}
-        src={src}
+        src={source}
       ></iframe>
     </div>
   );
--- src/screens/CovidCert/CovidCertPage.js
+++ src/screens/CovidCert/CovidCertPage.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import Input from '../../components/Input/Input';
 import css from './covid-styles.module.css';
-import Select from '../../components/Select/Select';
 import { Link, Redirect } from 'react-router-dom';
-import photo from '../../images/no_photo.jpg';
-import closeBtn from '../../images/close.svg';
-import Lines from '../../images/lines/profile.svg';
 
 import request from '../../request';
-import NewPhoneInput from '../../components/PhoneInput/PhoneInput';
-import Modal from '../../components/Modal/Modal';
 import Spinner from '../../components/Spinner/Spinner';
-import Isemail from 'isemail';
-import Loader from 'react-loader-spinner';
 import Back from '../../components/Back/Back';
 import CovidLogo from '../../images/covid-logo.png';
 import Button from '../../components/Button/Button';
+import { useQRCodeScan } from '../../lib/useQRCodeScan';
 
 // Импорты из конфига
-import { InstCode } from '../../config';
 import Iframe from '../../components/Iframe/Iframe';
 
 const CovidCertPage = () => {
   const [loading, setLoading] = useState(false);
+  const [isScanning, SetScanning] = useState(false);
+
+  const { startQrCode, decodedQRData, stopQrCode } = useQRCodeScan({
+    qrcodeMountNodeID: 'qrcodemountnode',
+  });
+
+  useEffect(() => {
+    // Add logic to add the camera and scan it
+    // startQrCode();
+  }, []);
+
+  console.log({ decodedQRData });
 
   if (loading) {
     return <Spinner show={loading} />;
@@ -49,8 +52,27 @@ const CovidCertPage = () => {
 
       <div className={css.buttons}>
         <span className={css.button}>Загрузить файл</span>
-        <span className={css.button}>Сфотографировать QR-код</span>
+        <span
+          className={css.button}
+          onClick={() => {
+            startQrCode();
+            SetScanning(true);
+          }}
+        >
+          Сфотографировать QR-код
+        </span>
       </div>
+
+      <div id='qrcodemountnode'></div>
+      {decodedQRData.isScanning && (
+        <button
+          onClick={() => {
+            stopQrCode();
+          }}
+        >
+          Остановить
+        </button>
+      )}
 
       <Iframe source='https://www.gosuslugi.ru/covid-cert/status/05434086-6142-4770-ac29-56279e3bca52?lang=ru' />
 
