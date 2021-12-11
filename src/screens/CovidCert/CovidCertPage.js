e69187afb906fae7a758e4ab47637891bbb54fa1
aduditsky <60918075+aduditsky@users.noreply.github.com>
2021-12-12 05:27:26 +0700

    GotIt

--- src/screens/CovidCert/CovidCertPage.js
+++ src/screens/CovidCert/CovidCertPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import css from './covid-styles.module.css';
-// import { Link, Redirect } from 'react-router-dom';
 
 import { useHistory } from 'react-router-dom';
 import request from '../../request';
